96708c5afe734a6595b6a3b0e7db1635490af501
edchell <edchellmayb@gmail.com>
2024-07-30 16:21:43 +0800

    a

--- records.php
+++ records.php
@@ -1,5 +1,12 @@
 
-          
+          <!-- DataTables CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+
+<!-- Bootstrap JS -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
        <div class="col-md-12">   
        <div class="panel panel-default">
         <div class="panel-heading">
