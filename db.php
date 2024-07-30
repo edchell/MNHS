487bb5521321aee425251621cc1bbca35dde93f7
edchell <edchellmayb@gmail.com>
2024-07-30 14:56:42 +0800

    Update db.php
    a

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "grading_db";
+$servername = "127.0.0.1";
+$username = "u510162695_grading_db";
+$password = "u510162695_grading_db";
+$dbname = "1Grading_db";
 
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
