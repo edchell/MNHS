52bb85537133c0fbd071eff991afbf30ba6511ee
edchell <edchellmayb@gmail.com>
2024-08-01 10:45:22 +0800

    a

--- view_students.php
+++ view_students.php
@@ -11,108 +11,146 @@
     ?>
         <input type="hidden" name="id" value="<?php echo $id ?>">
 
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>LRN:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['LRN_NO'] ?>
-            </div>
-        </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Name:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['LASTNAME'] . ", " . $row['FIRSTNAME'] . " " . $row['MIDDLENAME']; ?>
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>LRN:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['LRN_NO'] ?>
+                </div>
             </div>
         </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Gender:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['GENDER'] ?>
-            </div>
-        </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Date of Birth:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['DATE_OF_BIRTH'] ?>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Name:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['LASTNAME'] . ", " . $row['FIRSTNAME'] . " " . $row['MIDDLENAME']; ?>
+                </div>
             </div>
         </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Place of Birth:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['BIRTH_PLACE'] ?>
-            </div>
-        </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Address:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['ADDRESS'] ?>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Gender:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['GENDER'] ?>
+                </div>
             </div>
         </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Parent or Guardian:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['PARENT_GUARDIAN'] ?>
-            </div>
-        </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Parent or Guardian Address:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['P_ADDRESS'] ?>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Date of Birth:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['DATE_OF_BIRTH'] ?>
+                </div>
             </div>
         </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Intermediate Course Completed:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['INT_COURSE_COMP'] ?>
-            </div>
-        </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Total No. of Years:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['TOTAL_NO_OF_YEARS'] ?>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Place of Birth:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['BIRTH_PLACE'] ?>
+                </div>
             </div>
         </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>School Year:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['SCHOOL_YEAR'] ?>
-            </div>
-        </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>General Average:</label>
-            </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['GEN_AVE'] ?>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Address:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['ADDRESS'] ?>
+                </div>
             </div>
         </div>
-        <div class="row profile-row">
-            <div class="col-md-5 text-right">
-                <label>Curriculum Enrolled:</label>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Parent or Guardian:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['PARENT_GUARDIAN'] ?>
+                </div>
             </div>
-            <div class="col-md-7 text-left">
-                <?php echo $row['PROGRAM'] ?>
+        </div>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Parent or Guardian Address:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['P_ADDRESS'] ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Intermediate Course Completed:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['INT_COURSE_COMP'] ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Total No. of Years:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['TOTAL_NO_OF_YEARS'] ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>School Year:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['SCHOOL_YEAR'] ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>General Average:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['GEN_AVE'] ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="profile-section">
+            <div class="row">
+                <div class="col-md-5 text-right">
+                    <label>Curriculum Enrolled:</label>
+                </div>
+                <div class="col-md-7 text-left">
+                    <?php echo $row['PROGRAM'] ?>
+                </div>
             </div>
         </div>
 
