58ab0eef39bc6d4614781003c4656b458bf3017a
edchell <edchellmayb@gmail.com>
2024-08-02 20:03:40 +0800

    a

--- users.php
+++ users.php
@@ -58,8 +58,8 @@ success:function(data)
         <td><?php echo $row['FIRSTNAME']." ".$row['LASTNAME'] ?></td>
         <td><?php echo $row['USER'] ?></td>
         <td><?php echo $row['USER_TYPE'] ?></td>
-        <td><a data-toggle="modal" data-target="#edit_user" data-id="<?php echo $row['USER_ID'] ?>" id="getUser"><i class="fa fa-pencil-square" aria-hidden="true"></i> edit</a></td>
-        <td><a data-toggle="modal" data-id="<?php echo $row['USER_ID'] ?>" id="getUser"><i class="fa fa-delete-square" aria-hidden="true"></i> delete</a></td>
+        <td><a data-toggle="modal" data-target="#edit_user" data-id="<?php echo $row['USER_ID'] ?>" id="getUser"><i class="fa fa-pencil-square" aria-hidden="true"></i> edit</a>
+        <a data-toggle="modal" data-id="<?php echo $row['USER_ID'] ?>" id="getUser"><i class="fa fa-delete-square" aria-hidden="true"></i> delete</a></td>
       </tr>
       </form>
       <?php
