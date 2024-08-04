9340dbdd9826779a271fea6e20d186b658fed2c2
edchell <edchellmayb@gmail.com>
2024-08-05 07:09:42 +0800

    a

--- form137.php
+++ form137.php
@@ -293,7 +293,7 @@ $user = $_SESSION['ID'];
 		<style>
         table {
             border-collapse: collapse;
-            width: 150%;
+            width: 80%;
             font-size: 12px;
         }
         th, td {
