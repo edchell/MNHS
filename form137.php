c2f521819e39c0f62b1c24e588991242c8d38e31
edchell <edchellmayb@gmail.com>
2024-08-04 22:27:47 +0800

    a

--- form137.php
+++ form137.php
@@ -142,8 +142,30 @@ $user = $_SESSION['ID'];
 <div id='print'>
 <div style="margin-left:.5in;margin-right:.5in;margin-top:.1in;margin-bottom:.1in;line-height:1mm;">
 
-<center><img src="images/mnlogo.jpg" alt="images/mnlogo.jpg" width="40" height="30"></center>
-<center><img src="images/Deped-Logo.png" alt="images/Deped-Logo.png" width="40" height="30"></center>
+<img src="images/mnlogo.jpg" alt="images/mnlogo.jpg" width="40" height="30">
+<img src="images/Deped-Logo.png" alt="images/Deped-Logo.png" width="40" height="30">
+
+<style>
+body {
+  background-image: url("images/mnlogo.jpg");
+  background-repeat: no-repeat;
+  background-position: left top;
+  margin-right: 50px;
+  background-attachment: fixed;
+}
+</style>
+
+<style>
+body {
+  background-image: url("images/Deped-Logo.png");
+  background-repeat: no-repeat;
+  background-position: right top;
+  margin-right: 50px;
+  background-attachment: fixed;
+}
+</style>
+
+
 		<p><center><b>MNHS Student Grading System</b></center></p>
 
 		  </div>
