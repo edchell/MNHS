0e8b378c396a3472f32bc1ed49d88a5b9e9c784f
edchell <edchellmayb@gmail.com>
2024-08-04 13:17:39 +0800

    a

--- record.php
+++ record.php
@@ -147,7 +147,7 @@ success:function(data)
       <div class="col-md-7 text-right">
     <?php $query = mysqli_query($conn,"SELECT school_year FROM school_year where status='Yes'");
     while($sy = mysqli_fetch_assoc($query)){ ?>
-      <a class='btn btn-success' href="rms.php?page=addrecord&id=<?php echo $_GET['id'] ?>&sy=<?php echo $sy['school_year'] ?>&prog=<?php echo $_GET['prog'] ?>"><i class="fa fa-plus"> Add Record</i></a>
+      <button class='btn btn-success' href="rms.php?page=addrecord&id=<?php echo $_GET['id'] ?>&sy=<?php echo $sy['school_year'] ?>&prog=<?php echo $_GET['prog'] ?>"><i class="fa fa-plus"> Add Record</i></button>
       <?php
     } ?>
     </div>
