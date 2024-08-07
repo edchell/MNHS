4e35323c81cf7e0f1b778b946ac80a53cbaeb940
edchell <edchellmayb@gmail.com>
2024-08-07 09:17:09 +0800

    a

--- searchStudent.php
+++ searchStudent.php
@@ -1,5 +1,7 @@
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <style>
@@ -45,6 +47,7 @@
             font-size: 10px;
         }
     </style>
+
 <?php
 include 'db.php';
 
