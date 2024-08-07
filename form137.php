763be3a9f30941575e83d13412994b889d983374
edchell <edchellmayb@gmail.com>
2024-08-07 09:36:51 +0800

    a

--- form137.php
+++ form137.php
@@ -144,7 +144,7 @@ $user = $_SESSION['ID'];
 
 <background-position: left top;>
 
-<img style="position: absolute; margin-left: -330px;" src="images/mnlogo.jpg" alt="images/mnlogo.jpg" width="60" height="45">
+<img style="position: absolute; margin-left: -330px;" src="images/mnlogo.jpg" alt="images/mnlogo.jpg" width="150" height="150">
 <img src="images/Deped-Logo.png" alt="images/Deped-Logo.png" width="60" height="45">
 
 
