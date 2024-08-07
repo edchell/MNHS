631e7a76aa1bf9bbeb7da8ef18fa548b4de4dcfb
edchell <edchellmayb@gmail.com>
2024-08-07 09:41:28 +0800

    a

--- form137.php
+++ form137.php
@@ -145,7 +145,7 @@ $user = $_SESSION['ID'];
 <background-position: left top;>
 
 <img style="position: absolute; margin-left: -330px;margin-top: -80px;" src="images/mnlogo.jpg" alt="images/mnlogo.jpg" width="150" height="150">
-<img src="images/Deped-Logo.png" alt="images/Deped-Logo.png" width="60" height="45">
+<img style="position: absolute; margin-right: -330px;margin-top: -80px;" src="images/Deped-Logo.png" alt="images/Deped-Logo.png" width="150" height="150">
 
 
 		<p><center><b>MNHS Student Grading System</b></center></p>
