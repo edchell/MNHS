3c9c00b3a82da217a5829993571be7fc7a92a168
edchell <edchellmayb@gmail.com>
2024-08-22 13:53:36 +0800

    h

--- form137.php
+++ form137.php
@@ -305,9 +305,9 @@ $user = $_SESSION['ID'];
 		
 		<table style="border-collapse:collapse">
 		<tr>
+		<td style="width:60px;border:1px solid black;font-size:12px;"><center><b></b>Subjects</center></td>
+		<td style="width:150px;border:1px solid black;font-size:12px;"><center><b>Sem Final Grade</b></center></td>
 		<td style="width:60px;border:1px solid black;font-size:12px;"><center><b>Indicate if Subject is CORE, APPLIED, or SPECIALIZED</b></center></td>
-		<td style="width:150px;border:1px solid black;font-size:12px;"><center><b>Subjects</b></center></td>
-		<td style="width:60px;border:1px solid black;font-size:12px;"><center><b>Sem Final Grade</b></center></td>
 		<td style="width:10px;border:1px solid black;font-size:12px;"><center><b>Action<br>Taken</b></center></td>
 		</tr>
 		<?php
