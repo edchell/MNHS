d32449a3626b068b2020e6631d97f6d619424a32
edchell <edchellmayb@gmail.com>
2024-08-28 22:24:30 +0800

    f

--- grading/form137.php
+++ grading/form137.php
@@ -120,7 +120,6 @@ $user = $_SESSION['ID'];
 		  <center><p><b><h4>SENIOR HIGH SCHOOL STUDENT PERMANENT RECORD</h4></b></p></center>
 		  </div>
           </div>
-		  <p id="grade"><span class="highlight">GRADE</span> A+</p>
           <div class="row">
 		  <div class="col-md-12">
 
@@ -257,6 +256,23 @@ $user = $_SESSION['ID'];
 		<table style="border-collapse:collapse">
 		<tr>
 		<td style="width:150px;border:1px solid black;font-size:12px;"><center><b>Subjects</b></center></td>
+		 <!-- Add more rows as needed -->
+		 </table>
+        
+        <!-- Periodic Table -->
+        <table class="periodic-table">
+            <tr>
+                <td colspan="18"><b>Periodic Table</b></td>
+            </tr>
+            <!-- Example rows for periodic table -->
+            <tr>
+                <td>H</td><td>He</td><td>Li</td><td>Be</td><td>B</td><td>C</td><td>N</td><td>O</td><td>F</td><td>Ne</td>
+            </tr>
+            <tr>
+                <td>Na</td><td>Mg</td><td>Al</td><td>Si</td><td>P</td><td>S</td><td>Cl</td><td>Ar</td>
+            </tr>
+            <!-- Add more rows as needed -->
+        </table>
 		<td style="width:60px;border:1px solid black;font-size:12px;"><center><b>Final Rating</b></center></td>
 		<td style="width:60px;border:1px solid black;font-size:12px;"><center><b>Units Earned</b></center></td>
 		<td style="width:10px;border:1px solid black;font-size:12px;"><center><b>Action<br>Taken</b></center></td>
