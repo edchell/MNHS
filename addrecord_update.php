e1358b7f6fd8708c53b735c236f3fc2ca4b7fd83
edchell <edchellmayb@gmail.com>
2024-08-31 00:17:09 +0800

    a

--- addrecord_update.php
+++ addrecord_update.php
@@ -124,7 +124,38 @@
        <div class="row">
        <label class="col-md-4 te" for="school">School</label>
        <div class="col-md-6">
-         <input type="text" name="school" class="form-control" id ="school" value="<?php echo $_GET['SCHOOL'] ?>" required>
+         <input type="text" name="school" class="form-control" id ="school" value="<?php echo htmlspecialchars($row['SCHOOL']); ?>" required>
+         <?php
+include 'db.php'; 
+
+$id = $_GET['id'];
+
+$id = mysqli_real_escape_string($conn, $id);
+
+$sql = "SELECT * 
+        FROM student_year_info 
+        LEFT JOIN grade ON student_year_info.YEAR = grade.grade_id 
+        LEFT JOIN advisers ON student_year_info.ADVISER = advisers.adviser_id 
+        WHERE STUDENT_ID = '$id'";
+$result = mysqli_query($conn, $sql);
+
+if ($row = mysqli_fetch_assoc($result)) {
+    $syi = $row['SYI_ID'];
+
+    $sql1 = "SELECT * FROM student_info WHERE STUDENT_ID = '$id'";
+    $result1 = mysqli_query($conn, $sql1);
+
+    if ($row1 = mysqli_fetch_assoc($result1)) {
+        $sql3 = "SELECT * FROM program WHERE PROGRAM_ID = '".$row1['PROGRAM']."'";
+        $result3 = mysqli_query($conn, $sql3);
+
+        if ($row2 = mysqli_fetch_assoc($result3)) {
+?>
+<?php
+        }
+    }
+}
+?>
        </div>
        </div>
        <br>
