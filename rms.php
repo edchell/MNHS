0deb2b5158da5cd06b511ad2dcb3ae3393bcdea1
edchell <edchellmayb@gmail.com>
2024-09-02 23:41:41 +0800

    a

--- rms.php
+++ rms.php
@@ -133,8 +133,8 @@
             </div>
             <!-- /.navbar-collapse -->
         </nav>
-          </div>
-          
+
+
         <div id="page-wrapper">
 
             <div class="container-fluid">
@@ -159,6 +159,7 @@ if (!empty($page)) {
 
             </div>
         </div>
+    </div>
     <div class="modal fade" id="new_user" role="dialog">
     <div class="modal-dialog">
     
