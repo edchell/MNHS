b18610b1f289745c5c983c8ad1e36cff0280ef40
edchell <edchellmayb@gmail.com>
2024-09-11 22:36:48 +0800

    a

--- archive.php
+++ archive.php
@@ -83,7 +83,7 @@
         });
     });
 
-    function deleteUser(userId) {
+    function unarchivedUser(userId) {
     Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
