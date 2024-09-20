8a114c47589501b9db3391eda7f4cdc0b2e23e05
edchell <edchellmayb@gmail.com>
2024-09-20 23:43:55 +0800

    a

--- addrecord.php
+++ addrecord.php
@@ -168,7 +168,31 @@
        <div class="row">
        <label class="col-md-4 te" for="sec">Section</label>
        <div class="col-md-6">
-         <input type="text" name="sec" class="form-control" id ="sec"required>
+       <?php
+            include 'db.php'; 
+              $id = $_GET['id'];
+                $id = mysqli_real_escape_string($conn, $id);
+                  $sql = "SELECT * 
+                          FROM student_year_info 
+                          LEFT JOIN grade ON student_year_info.YEAR = grade.grade_id 
+                          LEFT JOIN advisers ON student_year_info.ADVISER = advisers.adviser_id 
+                          WHERE STUDENT_ID = '$id'";
+                  $result = mysqli_query($conn, $sql);
+                    if ($row = mysqli_fetch_assoc($result)) {
+                      $syi = $row['SYI_ID'];
+                        $sql1 = "SELECT * FROM student_info WHERE STUDENT_ID = '$id'";
+                          $result1 = mysqli_query($conn, $sql1);
+                            if ($row1 = mysqli_fetch_assoc($result1)) {
+                              $sql3 = "SELECT * FROM program WHERE PROGRAM_ID = '".$row1['PROGRAM']."'";
+                                $result3 = mysqli_query($conn, $sql3);
+                                  if ($row2 = mysqli_fetch_assoc($result3)) {
+          ?>
+         <input type="text" name="sec" class="form-control" value="<?php echo htmlspecialchars($row['SECTION']); ?>" readonly id ="sec"required>
+         <?php
+                }
+              }
+            }
+          ?>
        </div>
        </div>
        <br>
