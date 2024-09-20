a4911b55760282983fc777d50347f864969ef2a2
edchell <edchellmayb@gmail.com>
2024-09-20 23:46:04 +0800

    a

--- addrecord.php
+++ addrecord.php
@@ -235,7 +235,31 @@
        <div class="row">
        <label class="col-md-2 te" for="adviser">Adviser</label>
        <div class="col-md-6">
-         <input type="text" name="adviser" class="form-control" id ="adviser" >
+       <?php
+              include 'db.php'; 
+                $id = $_GET['id'];
+                $id = mysqli_real_escape_string($conn, $id);
+                $sql = "SELECT * 
+                        FROM student_year_info 
+                        LEFT JOIN grade ON student_year_info.YEAR = grade.grade_id 
+                        LEFT JOIN advisers ON student_year_info.ADVISER = advisers.adviser_id 
+                        WHERE STUDENT_ID = '$id'";
+                $result = mysqli_query($conn, $sql);
+                  if ($row = mysqli_fetch_assoc($result)) {
+                    $syi = $row['SYI_ID'];
+                      $sql1 = "SELECT * FROM student_info WHERE STUDENT_ID = '$id'";
+                      $result1 = mysqli_query($conn, $sql1);
+                        if ($row1 = mysqli_fetch_assoc($result1)) {
+                          $sql3 = "SELECT * FROM program WHERE PROGRAM_ID = '".$row1['PROGRAM']."'";
+                          $result3 = mysqli_query($conn, $sql3);
+                            if ($row2 = mysqli_fetch_assoc($result3)) {
+            ?>
+         <input type="text" name="adviser" class="form-control" id ="adviser" readonly value="<?php echo htmlspecialchars($row['ADVISER']); ?>">
+         <?php
+                  }
+                }
+              }
+            ?>
        </div>
        </div>
        </div>
@@ -243,7 +267,31 @@
         <div class="row">
        <label class="col-md-2 te" for="ra">Rank</label>
        <div class="col-md-6">
-         <input type="text" name="rank" class="form-control" id ="ra" >
+       <?php
+              include 'db.php'; 
+                $id = $_GET['id'];
+                $id = mysqli_real_escape_string($conn, $id);
+                $sql = "SELECT * 
+                        FROM student_year_info 
+                        LEFT JOIN grade ON student_year_info.YEAR = grade.grade_id 
+                        LEFT JOIN advisers ON student_year_info.ADVISER = advisers.adviser_id 
+                        WHERE STUDENT_ID = '$id'";
+                $result = mysqli_query($conn, $sql);
+                if ($row = mysqli_fetch_assoc($result)) {
+                  $syi = $row['SYI_ID'];
+                  $sql1 = "SELECT * FROM student_info WHERE STUDENT_ID = '$id'";
+                  $result1 = mysqli_query($conn, $sql1);
+                  if ($row1 = mysqli_fetch_assoc($result1)) {
+                    $sql3 = "SELECT * FROM program WHERE PROGRAM_ID = '".$row1['PROGRAM']."'";
+                    $result3 = mysqli_query($conn, $sql3);
+                    if ($row2 = mysqli_fetch_assoc($result3)) {
+            ?>
+         <input type="text" name="rank" class="form-control" id ="ra" value="<?php echo htmlspecialchars($row['RANK']); ?>" readonly>
+         <?php
+                  }
+                }
+              }
+            ?>
        </div>
        </div>
         <br>
