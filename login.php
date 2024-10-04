812592714d3d807204675e4035b4edb3f13edc08
edchell <edchellmayb@gmail.com>
2024-10-04 09:14:58 +0800

    a

--- login.php
+++ login.php
@@ -1,65 +1,117 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    include 'db.php'; // Assuming this includes your database connection
 
-    // Get and sanitize input
-    $id = $_POST['id'];
-    $fname = htmlspecialchars($_POST['fname'], ENT_QUOTES, 'UTF-8');
-    $lname = htmlspecialchars($_POST['lname'], ENT_QUOTES, 'UTF-8');
-    $user = htmlspecialchars($_POST['user'], ENT_QUOTES, 'UTF-8');
-    $pwd = $_POST['pwd'];
-    $type = $_POST['type'];
-    $current = $_POST['admin'];
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="images/Mad.jpg.jpg">
 
-    // Use prepared statements for security
-    $stmt = $conn->prepare("SELECT PASSWORD FROM user WHERE USER_ID = ?");
-    $stmt->bind_param("i", $id); // 'i' indicates the variable type is integer
-    $stmt->execute();
-    $result = $stmt->get_result();
+    <title>Student Grading System</title>
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="asset/css/style.css" rel="stylesheet">
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="assets/css/sticky-footer-navbar.css" rel="stylesheet">
+        <link href="asset/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-        // Verify the current password
-        if (password_verify($current, $row['PASSWORD'])) {
-            // Hash the new password if it's provided
-            if (!empty($pwd)) {
-                $hashed_password = password_hash($pwd, PASSWORD_BCRYPT);
-            } else {
-                // Use the existing password if no new password is provided
-                $hashed_password = $row['PASSWORD'];
-            }
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.js"></script>
 
-            // Update user information
-            $update_stmt = $conn->prepare("UPDATE user SET FIRSTNAME = ?, LASTNAME = ?, USER = ?, PASSWORD = ?, USER_TYPE = ? WHERE USER_ID = ?");
-            $update_stmt->bind_param("sssssi", $fname, $lname, $user, $hashed_password, $type, $id); // 's' for string, 'i' for integer
-
-            if ($update_stmt->execute()) {
-                echo "<script>
-                    alert('Account updated successfully');
-                    location.replace(document.referrer);
-                </script>";
-            } else {
-                echo "<script>
-                    alert('Failed to update account. Please try again.');
-                    location.replace(document.referrer);
-                </script>";
-            }
-        } else {
-            echo "<script>
-                alert('Current password is incorrect.');
-                location.replace(document.referrer);
-            </script>";
-        }
-    } else {
-        echo "<script>
-            alert('User not found.');
-            location.replace(document.referrer);
-        </script>";
-    }
-
-    $stmt->close();
-    $update_stmt->close();
-    mysqli_close($conn);
+    
+    <style>
+      body{
+        display: flex;
+        height: calc(100%);
+        width: calc(100%);
+        justify-content: center;
+        align-items: center;
+        background-image: url('images/Mad.jpg.jpg');
+        background-repeat: no-repeat; /* Prevent tiling */
+        background-size: contain; /* Scale the image to fit within the container */
+        background-position: center; /* Center the image */
+        width: 100%; /* Set the width of the container */
+        height: 600px; 
+      }
+      .login-form {
+  display: block;
+  position: fixed;
+  border:2px solid grey;
+  border-radius: 10px;
+  padding: 30px;
+  width: 500px;
+  background-color: transparent;
+  left:500px;
+  top:110px;
+ } 
+ .erlert{
+  display:block;
+  border-radius:5px;
+  background-color:rgba(230, 103, 42, 0.37);
+  padding: 5px;
 }
-?>
+    </style>
+  </head>
+<body>
+  
+
+<div class="container-fluid">
+
+  <div class="login-form" id="login_modal" role="dialog" >
+
+
+  
+
+  <form class="form-horizontal" id="loginForm" method="post">
+    <div class="form-group">
+      <center>  <label class="control-label col-sm-2" for="user">User:</label> </center>
+        <div class="col-md-10">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                <input type="text" class="form-control" id="user" name="user" placeholder="Enter User" autocomplete="off">
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+    <center>   <label class="control-label col-sm-2" for="pwd">Password:</label> </center>
+        <div class="col-md-10">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-key fa" aria-hidden="true"></i></span>
+                <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Enter password" autocomplete="off">
+            </div>
+        </div>
+    </div>
+    <div class="form-group">        
+        <div class="col-md-offset-6 col-md-9">
+          <button type="submit" class="btn btn-default">Login</button>
+          <a href="view2.php" class="btn btn-primary">Student View</a> 
+        </div>
+    </div>
+</form>
+
+  <?php if (isset($error_message)): ?>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <?php echo $error_message; ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            <?php endif; ?>
+   <?php
+  include 'connect.php';
+  ?>
+   </div>          
+</div>
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  
+</html>
