b6b6ff6c9c287c7a0ec51f5120d8f94025a65e6e
Black Mann <caranzocrisa@gmail.com>
2024-10-14 21:42:09 +0800

    commit

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
 <?php
-$host = "127.0.0.1";
-//$user = "u510162695_grading_db";
-//$password = "1Grading_db";
-//$database = "u510162695_grading_db";
-$user = "root";
-$password = "";
-$database = "grading_db";
+$host = "localhost";
+$user = "u510162695_grading_db";
+$password = "1Grading_db";
+$database = "u510162695_grading_db";
+// $user = "root";
+// $password = "";
+// $database = "grading_db";
 
 // Create connection
 $conn = new mysqli($host, $user, $password, $database);
