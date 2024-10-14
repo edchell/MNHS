23f45ba9aa3406701d6ff3571309c74422759a93
Black Mann <caranzocrisa@gmail.com>
2024-10-14 22:04:45 +0800

    commit

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,12 +3,6 @@ session_start();
 include('../includes/header.php');
 include('include/topnav.php');
 include('include/sidebar.php');
-
-// Check if the user is logged in
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ../index.php");
-    exit();
-}
 ?>
 
 <div class="main-container">
@@ -18,83 +12,94 @@ if (!isset($_SESSION['user_id'])) {
         </div>
         <div class="row pb-10">
             <div class="col-xl-4 col-lg-3 col-md-6 mb-20">
-                <div class="card-box height-100-p widget-style3">
-                    <div class="d-flex flex-wrap">
-                        <div class="widget-data">
-                            <div class="weight-700 font-24 text-dark"><?php echo $totalUsers; ?></div>
-                            <div class="font-14 text-secondary weight-500">Users</div>
-                        </div>
-                        <div class="widget-icon">
-                            <div class="icon" data-color="#00eccf">
-                                <i class="icon-copy fa fa-users"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+				<div class="card-box height-100-p widget-style3">
+				    <div class="d-flex flex-wrap">
+						<div class="widget-data">
+							<div class="weight-700 font-24 text-dark">75</div>
+							<div class="font-14 text-secondary weight-500">
+								Users
+							</div>
+						</div>
+						<div class="widget-icon">
+							<div class="icon" data-color="#00eccf">
+								<i class="icon-copy fa fa-users"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
             <div class="col-xl-4 col-lg-3 col-md-6 mb-20">
-                <div class="card-box height-100-p widget-style3">
-                    <div class="d-flex flex-wrap">
-                        <div class="widget-data">
-                            <div class="weight-700 font-24 text-dark"><?php echo $totalMale; ?></div>
-                            <div class="font-14 text-secondary weight-500">Total Male Students</div>
-                        </div>
-                        <div class="widget-icon">
-                            <div class="icon" data-color="#00eccf">
-                                <i class="icon-copy fa fa-male"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+				<div class="card-box height-100-p widget-style3">
+				    <div class="d-flex flex-wrap">
+						<div class="widget-data">
+							<div class="weight-700 font-24 text-dark">75</div>
+							<div class="font-14 text-secondary weight-500">
+								Total Male Students
+							</div>
+						</div>
+						<div class="widget-icon">
+							<div class="icon" data-color="#00eccf">
+								<i class="icon-copy fa fa-male"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
             <div class="col-xl-4 col-lg-3 col-md-6 mb-20">
-                <div class="card-box height-100-p widget-style3">
-                    <div class="d-flex flex-wrap">
-                        <div class="widget-data">
-                            <div class="weight-700 font-24 text-dark"><?php echo $totalFemale; ?></div>
-                            <div class="font-14 text-secondary weight-500">Total Female Students</div>
-                        </div>
-                        <div class="widget-icon">
-                            <div class="icon" data-color="#00eccf">
-                                <i class="icon-copy fa fa-female"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+				<div class="card-box height-100-p widget-style3">
+				    <div class="d-flex flex-wrap">
+						<div class="widget-data">
+							<div class="weight-700 font-24 text-dark">75</div>
+							<div class="font-14 text-secondary weight-500">
+								Total Female Students
+							</div>
+						</div>
+						<div class="widget-icon">
+							<div class="icon" data-color="#00eccf">
+								<i class="icon-copy fa fa-female"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
             <div class="col-xl-6 col-lg-3 col-md-6 mb-20">
-                <div class="card-box height-100-p widget-style3">
-                    <div class="d-flex flex-wrap">
-                        <div class="widget-data">
-                            <div class="weight-700 font-24 text-dark"><?php echo $totalStudents; ?></div>
-                            <div class="font-14 text-secondary weight-500">Total Students</div>
-                        </div>
-                        <div class="widget-icon">
-                            <div class="icon" data-color="#00eccf">
-                                <i class="icon-copy fi-torso-business"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+				<div class="card-box height-100-p widget-style3">
+				    <div class="d-flex flex-wrap">
+						<div class="widget-data">
+							<div class="weight-700 font-24 text-dark">75</div>
+							<div class="font-14 text-secondary weight-500">
+								Total Students
+							</div>
+						</div>
+						<div class="widget-icon">
+							<div class="icon" data-color="#00eccf">
+								<i class="icon-copy fi-torso-business"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
             <div class="col-xl-6 col-lg-3 col-md-6 mb-20">
-                <div class="card-box height-100-p widget-style3">
-                    <div class="d-flex flex-wrap">
-                        <div class="widget-data">
-                            <div class="weight-700 font-24 text-dark"><?php echo $totalSubjects; ?></div>
-                            <div class="font-14 text-secondary weight-500">Subjects</div>
-                        </div>
-                        <div class="widget-icon">
-                            <div class="icon" data-color="#00eccf">
-                                <i class="icon-copy fa fa-book"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+				<div class="card-box height-100-p widget-style3">
+				    <div class="d-flex flex-wrap">
+						<div class="widget-data">
+							<div class="weight-700 font-24 text-dark">75</div>
+							<div class="font-14 text-secondary weight-500">
+								Subjects
+							</div>
+						</div>
+						<div class="widget-icon">
+							<div class="icon" data-color="#00eccf">
+								<i class="icon-copy fa fa-book"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
         </div>
     </div>
 </div>
+
 
 <?php
 include('../includes/footer.php');
