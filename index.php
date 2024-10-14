98e1fab4b271c8aca1760d5947d7fd226581ebf8
Black Mann <caranzocrisa@gmail.com>
2024-10-14 20:56:26 +0800

    commit

--- index.php
+++ index.php
@@ -2,89 +2,58 @@
 include('includes/header.php');
 ?>
 
-<div class="login-header box-shadow">
-			
-		</div>
-		<div
-			class="login-wrap d-flex align-items-center flex-wrap justify-content-center"
-		>
-			<div class="container">
-				<div class="row align-items-center">
-					<div class="col-md-6 col-lg-7">
-					</div>
-					<div class="col-md-6 col-lg-5">
-						<div class="login-box bg-white box-shadow border-radius-10">
-							<div class="login-title">
-								<h2 class="text-center text-primary">Login</h2>
-							</div>
-							<form action="login_code.php" method="post">
-								<div class="input-group custom">
-                                    <input
-                                        type="email"
-                                        class="form-control form-control-lg"
-                                        name="email" 
-                                        placeholder="Email"
-                                    />
-                                    <div class="input-group-append custom">
-                                        <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
-                                    </div>
+<div class="login-header box-shadow"></div>
+<div class="login-wrap d-flex align-items-center flex-wrap justify-content-center">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6 col-lg-7"></div>
+            <div class="col-md-6 col-lg-5">
+                <div class="login-box bg-white box-shadow border-radius-10">
+                    <div class="login-title">
+                        <h2 class="text-center text-primary">Login</h2>
+                    </div>
+                    <form action="login_code.php" method="post">
+                        <div class="input-group custom">
+                            <input type="email" class="form-control form-control-lg" name="email" placeholder="Email" required />
+                            <div class="input-group-append custom">
+                                <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+                            </div>
+                        </div>
+                        <div class="input-group custom">
+                            <input type="password" class="form-control form-control-lg" name="password" placeholder="**********" required />
+                            <div class="input-group-append custom">
+                                <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
+                            </div>
+                        </div>
+                        <div class="row pb-30">
+                            <div class="col-6">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck1" />
+                                    <label class="custom-control-label" for="customCheck1">Remember</label>
                                 </div>
-								<div class="input-group custom">
-                                    <input
-                                        type="password"
-                                        class="form-control form-control-lg"
-                                        name="password"
-                                        placeholder="**********"
-                                    />
-                                    <div class="input-group-append custom">
-                                        <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
-                                    </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="forgot-password">
+                                    <a href="forgot-password.html">Forgot Password</a>
                                 </div>
-								<div class="row pb-30">
-									<div class="col-6">
-										<div class="custom-control custom-checkbox">
-											<input
-												type="checkbox"
-												class="custom-control-input"
-												id="customCheck1"
-											/>
-											<label class="custom-control-label" for="customCheck1"
-												>Remember</label
-											>
-										</div>
-									</div>
-									<div class="col-6">
-										<div class="forgot-password">
-											<a href="forgot-password.html">Forgot Password</a>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-sm-12">
-										<div class="input-group mb-2">
-											<input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
-										
-											<!-- <a
-												class="btn btn-primary btn-lg btn-block"
-												href="admin/dashboard.php"
-												>Sign In</a
-											> -->
-										</div>
-										<div class="input-group mb-0">
-											<a
-												class="btn btn-outline-primary btn-lg btn-block"
-												href="register.html"
-												>Students View</a
-											>
-										</div>
-									</div>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="input-group mb-2">
+                                    <input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
+                                </div>
+                                <div class="input-group mb-0">
+                                    <a class="btn btn-outline-primary btn-lg btn-block" href="register.html">Students View</a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <script>
 document.querySelector('form').onsubmit = function(event) {
@@ -120,7 +89,6 @@ document.querySelector('form').onsubmit = function(event) {
     });
 };
 </script>
-
 
 <?php
 include('includes/footer.php');
