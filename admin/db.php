ada03096ea9db6775edf54e9147a938e414cafae
Black Mann <caranzocrisa@gmail.com>
2024-10-14 21:18:18 +0800

    commit

--- admin/db.php
+++ admin/db.php
@@ -2,10 +2,10 @@
 error_reporting(E_ALL ^ E_NOTICE);
 
 // Initialize the page variable
-$page = isset($_GET['page']) ? $_GET['page'] : 'dashboard'; // Default to 'dashboard'
+$page = isset($_GET['page']) ? $_GET['page'] : 'index'; // Default to 'index'
 
 // Define allowed pages
-$pages = array('dashboard', 'users', 'student_list', 'student_profile', 'subject_list', 'academic_record', 'academic_view');
+$pages = array('index', 'dashboard', 'users', 'student_list', 'student_profile', 'subject_list', 'academic_record', 'academic_view');
 
 // Check if the requested page is in the allowed list
 if (in_array($page, $pages)) {
@@ -14,6 +14,6 @@ if (in_array($page, $pages)) {
 } else {
     // Page not found, show an error message
     echo 'Page not found. Return to 
-    <a href="db.php?page=dashboard">dashboard</a>';
+    <a href="db.php?page=index">index</a>';
 }
 ?>
