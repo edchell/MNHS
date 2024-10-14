43e69663d38c3737d07a86a38a7d0d0d18f47549
Black Mann <caranzocrisa@gmail.com>
2024-10-14 15:03:20 +0800

    commit

--- admin/db.php
+++ admin/db.php
@@ -1,17 +1,22 @@
 <?php
+session_start(); // Start the session
+
 error_reporting(E_ALL ^ E_NOTICE);
 
-$page = $_GET['page'];
+$page = isset($_GET['page']) ? $_GET['page'] : '';
 $pages = array('dashboard', 'users', 'student_list', 'student_profile', 'subject_list', 'academic_record', 'academic_view');
-if (!empty($page)) {
-    if(in_array($page,$pages)) {
-        $page .= '.php';
-        include($page);
-    }
-    else {
-        echo 'Page not found. Return
-        <a href="db.php?page=dashboard">dashboard</a>';
-    }
+
+// Check if the user is not logged in
+if (!isset($_SESSION['USER'])) { 
+    header("HTTP/1.0 404 Not Found"); // Set the response status to 404
+    include("404.php"); // Include your 404 page
+    exit();
 }
 
+if (!empty($page) && in_array($page, $pages)) {
+    $page .= '.php';
+    include($page);
+} else {
+    echo 'Page not found. Return <a href="db.php?page=dashboard">dashboard</a>';
+}
 ?>
