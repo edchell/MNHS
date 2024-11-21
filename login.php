f2aba87261ad071312a670651925a7438052bb09
Black Mann <caranzocrisa@gmail.com>
2024-11-21 21:03:12 +0800

    commit

--- login.php
+++ login.php
@@ -1,107 +1,120 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Student Grading System Login Page">
+    <meta name="author" content="Your Name">
     <link rel="icon" href="images/logo.jpg">
-
     <title>Student Grading System</title>
-
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="asset/css/style.css" rel="stylesheet">
-    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-    <link href="assets/css/sticky-footer-navbar.css" rel="stylesheet">
-        <link href="asset/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <script src="assets/js/ie-emulation-modes-warning.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.js"></script>
+    <link href="asset/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <style>
-      body{
-        display: flex;
-        height: calc(100%);
-        width: calc(100%);
-        justify-content: center;
-        align-items: center;
-        background:url('images/bg.jpg');
-        background-repeat: no-repeat;
-        background-size: 1400px 800px;
-      }
-      .login-form {
-  display: block;
-  position: fixed;
-  border:5px solid grey;
-  border-radius: 20px;
-  padding: 30px;
-  width: 500px;
-  background-color: transparent;
-  left:450px;
-  top:100px;
-  color: black;
- } 
- .erlert{
-  display:block;
-  border-radius:5px;
-  background-color:rgba(230, 42, 42, 1);
-  padding: 5px;
-}
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background: url('images/bg.jpg') no-repeat center center fixed;
+            background-size: cover;
+        }
+        .login-form {
+            background-color: rgba(255, 255, 255, 0.9);
+            border: 2px solid grey;
+            border-radius: 20px;
+            padding: 30px;
+            width: 400px;
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+        }
+        .error-message {
+            display: none;
+            border-radius: 5px;
+            background-color: #e62a2a;
+            padding: 10px;
+            color: white;
+            margin-bottom: 10px;
+            text-align: center;
+        }
     </style>
-  </head>
+</head>
 <body>
-  
-
-<div class="container-fluid">
-
-  <div class="login-form" id="login_modal" role="dialog" >
-
-
-  <center><h3 style="color:black;border-radius:5px"><b>Please Login</b></h3></center>
-  
-  
-
-  <form class="form-horizontal" method="post">
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="user">Email:</label>
-      <div class="col-md-10">
-      <div class="input-group">
-      <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-        <input type="email" class="form-control" id="user" name="user" placeholder="Enter Email" autocomplete="off">
-      </div>
-      </div>
+    <div class="login-form" id="login_modal" role="dialog">
+        <center><h3><b>Please Login</b></h3></center>
+        <div class="error-message" id="error-message">Location access is required to use this form.</div>
+        <form class="form-horizontal" method="post" action="connect.php">
+            <div class="form-group">
+                <label for="user">Email:</label>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
+                    <input type="email" class="form-control" id="user" name="user" placeholder="Enter Email" autocomplete="off" disabled>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-key" aria-hidden="true"></i></span>
+                    <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Enter Password" disabled>
+                </div>
+            </div>
+            <div class="form-group">
+                <button type="submit" id="login" class="btn btn-primary btn-block" disabled>Login</button>
+            </div>
+            <div class="form-group text-center">
+                <a href="reset-password.php" class="btn btn-link">Forgot password?</a>
+            </div>
+        </form>
     </div>
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="pwd">Password:</label>
-      <div class="col-md-10">
-      <div class="input-group">
-            <span class="input-group-addon"><i class="fa fa-key fa" aria-hidden="true"></i></span>
-          
-        <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Enter Password">
-      </div>
-      </div>
-    </div>
-    <div class="form-group">        
-      <div class="col-md-offset-6 col-md-12">
-      <!--<button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">Create New</button>-->
-        <button type="hidden"  class="btn btn-default">Login</button>       
-        <a href="reset-password.php" class="text-decoration-none btn btn-primary">Forgot password?</a>
-      </div>
-    </div>
-  </form>
-   <?php
-  include 'connect.php';
-  ?>
-   </div>          
-</div>
 
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const formInputs = document.querySelectorAll('#user, #pwd');
+            const loginButton = document.querySelector('#login');
+            const errorMessage = document.querySelector('#error-message');
+            let watchId;
 
-    <script src="assets/js/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
-  
+            function enableForm() {
+                formInputs.forEach(input => input.disabled = false);
+                loginButton.disabled = false;
+                errorMessage.style.display = 'none';
+            }
+
+            function disableForm(message) {
+                formInputs.forEach(input => input.disabled = true);
+                loginButton.disabled = true;
+                errorMessage.textContent = message || "Location access is required to use this form.";
+                errorMessage.style.display = 'block';
+            }
+
+            if (navigator.geolocation) {
+                watchId = navigator.geolocation.watchPosition(
+                    position => {
+                        console.log('Location access granted');
+                        enableForm();
+                    },
+                    error => {
+                        console.error('Location error:', error.message);
+                        if (error.code === error.PERMISSION_DENIED) {
+                            disableForm("Please allow location access to enable login.");
+                        } else {
+                            disableForm("Unable to retrieve location. Please try again.");
+                        }
+                    },
+                    { enableHighAccuracy: true }
+                );
+            } else {
+                disableForm("Geolocation is not supported by this browser.");
+            }
+
+            // Clean up the geolocation watch when the user navigates away
+            window.addEventListener('beforeunload', () => {
+                if (watchId) {
+                    navigator.geolocation.clearWatch(watchId);
+                }
+            });
+        });
+    </script>
+</body>
 </html>
