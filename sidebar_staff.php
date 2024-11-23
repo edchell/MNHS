0748d753122c9de064081d8a074fdb0bfb0347d6
Black Mann <caranzocrisa@gmail.com>
2024-11-23 22:26:08 +0800

    commit

--- sidebar_staff.php
+++ sidebar_staff.php
@@ -11,10 +11,10 @@ $school_year=$s['school_year'];
 
 
  <ul class="nav navbar-nav side-nav">
- <li>
+ <li style="background-color: transparent;">
 <a href="rms.php?page=home"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
 </li>
-<li>
+<li style="background-color: transparent;">
 <a id=demo1 href="javascript:void(0)" data-toggle="collapse" data-target="#masterlistCollapse"><i class="fa fa-fw fa-files-o"></i> Master List <i class="fa fa-fw fa-caret-down"></i></a>
 <ul id="masterlistCollapse" class="collapse">
 <li style="background-color: transparent;">
@@ -40,7 +40,7 @@ $school_year=$s['school_year'];
     </li>
 </ul>
 </li>
-<li>
+<li style="background-color: transparent;">
     <a href="javascript:void(0)" data-toggle="collapse" data-target="#recordsCollapse"><i class="fa fa-fw fa-file"></i> Records       <i class="fa fa-fw fa-caret-down"></i></a>
     <ul class="collapse" id="recordsCollapse">
         <li>
@@ -48,13 +48,13 @@ $school_year=$s['school_year'];
         </li>
     </ul>
 </li>
-<li>
+<li style="background-color: transparent;">
     <a href="javascript:void(0)" data-toggle="collapse" data-target="#reportsCollapse"><i class="fa fa-fw fa-file"></i> Reports       <i class="fa fa-fw fa-caret-down"></i></a>
     <ul id="reportsCollapse" class="collapse">
-        <li>
+        <li style="background-color: transparent;">
             <a href="rms.php?page=report"><i class="fa fa-fw fa-files-o"></i> Form 137</a>
         </li>
-        <li>
+        <li style="background-color: transparent;">
             <a href="#" data-toggle="modal" data-target="#s_report"><i class="fa fa-fw fa-files-o"></i> Students List</a>
         </li>
     </ul>
