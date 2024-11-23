32c7db3583145aebcd4f8a205e5169022656d32e
Black Mann <caranzocrisa@gmail.com>
2024-11-23 22:34:15 +0800

    commit

--- newrecord.php
+++ newrecord.php
@@ -72,7 +72,7 @@ if ($num_row >= 1) {
     }
 
     // Insert notification after the new student record is added
-    $users_query = mysqli_query($conn, "SELECT USER_ID FROM user WHERE USER_ID != '$user' AND STATUS = ''");
+    $users_query = mysqli_query($conn, "SELECT USER_ID FROM user WHERE USER_ID = 'ADMINISTRATOR' AND STATUS = ''");
 while ($user = mysqli_fetch_assoc($users_query)) {
     $user_id = $user['USER_ID'];
 
