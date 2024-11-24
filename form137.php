1a53ebcce673840e6bd519731a754e653efc7429
Black Mann <caranzocrisa@gmail.com>
2024-11-24 15:24:50 +0800

    commit

---
+++ form137.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="images/logo.jpg">
+    <title>MNHS - Student Grading System</title>
+    <link href="asset/css/bootstrap.min.css" rel="stylesheet">
+    <link href="asset/css/styles.css" rel="stylesheet">
+    <link href="asset/css/sb-admin.css" rel="stylesheet">
+    <link href="asset/css/plugins/morris.css" rel="stylesheet">
+    <link href="asset/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <script src="datatables/jquery.dataTables.js"></script>
+    <script src="datatables/dataTables.bootstrap.js"></script>
+    <link href="datatables/dataTables.bootstrap.css" rel="stylesheet">
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="asset/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+    <script src="assets/js/jq.js"></script>
+</head>
+<body>
+	<div class="printContainer">
+		<div class="left-head">
+			<p>DepEd Form 137-A</p>
+			<p>LRN No.</p>
+		</div>
+	</div>
+</body>
+</html>
