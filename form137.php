ac7efa0a30e917101183aac584a97e1a20bd30c7
Black Mann <caranzocrisa@gmail.com>
2024-11-24 19:24:00 +0800

    a

--- form137.php
+++ form137.php
@@ -282,7 +282,7 @@ if (strpos($request, '.php') !== false) {
 				</span>
 			</div>
 		</div>
-        <table style="width: 90%">
+        <table style="width: 75%">
             <tr>
                 <td style="width:150px;border:1px solid black;font-size:10px;height:15px;text-align:center;"><b>Subjects</b></td>
                 <td style="width:60px;border:1px solid black;font-size:10px;height:15px;text-align:center"><b>Final Rating</b></td>
