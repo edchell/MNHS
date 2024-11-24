00923328bc6ccb5f757f7030431e48216037c332
Black Mann <caranzocrisa@gmail.com>
2024-11-24 17:10:55 +0800

    commit

--- form137.php
+++ form137.php
@@ -206,14 +206,14 @@ if (strpos($request, '.php') !== false) {
 				</span>
 			</div>
 		</div>
-        <div class="pob">
-			<div>
+        <div class="dob">
+			<div class="pob">
 				<label><h6 style="font-size:12px;">Parents/Guardian</h6></label>
 				<span>
 					<label for=""><p style="font-weight:bold;border-bottom:1px solid black;width:300px;"><?php echo $row['PARENT_GUARDIAN']?></p></label>
 				</span>
 			</div>
-			<div>
+			<div class="pob">
 				<label><h6 style="font-size:12px;">Address of Parents/Guardian</h6></label>
 				<span>
 					<label for=""><p style="font-weight:bold;border-bottom:1px solid black;width:300px;"><?php echo $row['P_ADDRESS']?></p></label>
