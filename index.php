d4ac5337228fc262cbdcad9c37306636a8bad0b2
Black Mann <caranzocrisa@gmail.com>
2024-12-01 06:08:22 +0800

    commit

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ if (isset($_SESSION['login_success']) && $_SESSION['login_success']) {
                             $lockout_time_remaining = $_SESSION['lockout_time'] - time();
                             $minutes_remaining = ceil($lockout_time_remaining / 60);
                             ?>
-                            <div class="alert alert-danger">
+                            <div class="error-message">
                                 Too many failed attempts. Please try again later.
                             </div>
                         <?php endif; ?>
