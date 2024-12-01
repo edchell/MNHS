25c3e7307aeba9f98ca2226e13a57ffbf550a535
Black Mann <caranzocrisa@gmail.com>
2024-12-01 14:22:14 +0800

    commit

--- new_grade.php
+++ new_grade.php
@@ -1,56 +1,62 @@
-<?php 
+<?php
+include('auth.php');
+include('db.php');
 
-	include 'db.php';
-	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $errors = array();
 
-	$errors = array();
+    // Validation for the grade input
+    if (preg_match("/\S+/", $_POST['grade']) === 0) {
+        $errors['grade'] = "* Grade is required.";
+    }
 
-	if(preg_match("/\S+/", $_POST['grade']) === 0){
-		$errors['grade'] = "* Grade is required.";
-	}
-	
-	if(count($errors) === 0){
+    if (count($errors) === 0) {
+        $grade = $_POST['grade'];
+        $user = $_SESSION['ID'];
 
+        if ($_POST['id'] == "") {
+            // Add new grade
+            $sql = "INSERT INTO grade (grade) VALUES (?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $grade);
 
-	$grade=$_POST['grade'];
-	$user = $_SESSION['ID'];
-	
-	if($_POST['id'] == ""){
+            if ($stmt->execute()) {
+                $log_sql = "INSERT INTO history_log (transaction, user_id, date_added) VALUES (?, ?, NOW())";
+                $log_stmt = $conn->prepare($log_sql);
+                $log_stmt->bind_param("si", "added $grade to the grades list", $user);
+                $log_stmt->execute();
 
-	if ($sql=mysqli_query($conn, "INSERT into grade (grade) 
-		VALUES ( '$grade' )") && $sql2=mysqli_query($conn, "INSERT into history_log (transaction,user_id,date_added) 
-		VALUES ('added $grade in the grades list','$user',NOW() )") ){
-	echo "<div class='erlert-success col-sm-12 col-sm-offset-2' style='width:300px;z-index:1000;position:fixed;left:500'><center><h4>New Grade Successfully Added.</h4></center></div>";
-	echo "<script>
-	setTimeout(function(){	window.location.href='rms.php?page=grade';  }, 1500);</script>";
-	} else {
-		echo "<script>
-		alert('New subject failed to record!" .$sql."');
-		window.location.href='rms.php?page=grade';
-		</script>";
-		unset($_POST);
+                echo 'success';
+            } else {
+                echo 'error';
+            }
 
-	}
-	}else{
-		$id=$_POST['id'];
-		$sql = "UPDATE grade SET grade='$grade' WHERE grade_id='$id'";
-		$sql2=mysqli_query($conn, "INSERT into history_log (transaction,user_id,date_added) 
-		VALUES ('updated $id in the grades list','$user',NOW() )");
-		if (mysqli_query($conn, $sql)) {
-			echo "<div class='erlert-success col-sm-12 col-sm-offset-2' style='width:300px;z-index:1000;position:fixed;left:500'><center><h4>Current School Year Successfully Updated.</h4></center></div>";
-			echo "<script>
-			setTimeout(function(){	window.location.href='rms.php?page=grade';  }, 1500);</script>";
+            $stmt->close();
+        } else {
+            // Update existing grade
+            $id = $_POST['id'];
+            $sql = "UPDATE grade SET grade = ? WHERE grade_id = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("si", $grade, $id);
 
-		} else {
-    echo "Error updating record: " . mysqli_error($conn);
-		}
-	}
-}else{
-	echo "<script>setTimeout(function(){	$('.erlert').hide()  }, 1500);</script>";
+            if ($stmt->execute()) {
+                $log_sql = "INSERT INTO history_log (transaction, user_id, date_added) VALUES (?, ?, NOW())";
+                $log_stmt = $conn->prepare($log_sql);
+                $log_stmt->bind_param("si", "updated $id in the grades list", $user);
+                $log_stmt->execute();
+
+                echo 'success';
+            } else {
+                echo 'error';
+            }
+
+            $stmt->close();
+        }
+    } else {
+        echo 'error';
+    }
+
+    $conn->close();
+    exit;
 }
-
-}
-
-	mysqli_close($conn);
-
- ?>
+?>
