379e14ba8d48650c1bc5068d24b997e5e1742bca
Black Mann <caranzocrisa@gmail.com>
2024-12-01 12:59:28 +0800

    commit

--- newaccount.php
+++ newaccount.php
@@ -14,12 +14,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (mysqli_query($conn, $sql)) {
         echo "<script>
+            alert('New account successfully recorded!');
+            location.href = 'rms.php?page=users'; // Fixed typo here
+        </script>";
+        echo "<script>
 					Swal.fire({
 						icon: 'success',
 						title: 'Success',
 						text: 'New account successfully recorded.',
 					}).then(() => {
-						window.location.href = 'rms.php?page=users';
+						location.href = 'rms.php?page=users';
 					});
 				</script>";
     } else {
