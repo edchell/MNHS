ff86773177ebb4886fe8fd265bca3a2a2f54ee73
Black Mann <caranzocrisa@gmail.com>
2024-12-03 00:59:48 +0800

    commit

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ if (isset($_SESSION['login_success']) && $_SESSION['login_success']) {
         }
         #termsModal {
             display: none; /* Hidden by default */
-            position: relative; /* Stay in place */
+            position: absolute; /* Stay in place */
             z-index: 1; /* Sit on top */
             left: 0;
             top: 0;
