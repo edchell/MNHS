f4922f3e34313e116069b8f3e907be459dd419cc
Black Mann <caranzocrisa@gmail.com>
2024-12-02 20:40:12 +0800

    commit

--- reset-pass-choose.php
+++ reset-pass-choose.php
@@ -71,7 +71,7 @@ if (strpos($request, '.php') !== false) {
             text-decoration: underline;
         }
         .reset-link {
-            box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.3);
+            box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);
         }
     </style>
 </head>
